7fb9347a9a80a5bf95df25ee0a944a15eaa9d15f
jonathanopigez <101323837+jonathanopigez@users.noreply.github.com>
2022-04-07 16:21:51 +0200

    ExoJS

---
+++ exoJS/Mon_1er_code.js
@@ -0,0 +1,13 @@
+let nombreLigne;
+let affichage = "";
+let nombre;
+
+nombreLigne = prompt("saisir le nombre de lignes");
+nombre = prompt("veuillez saisir un nombre ou un caractère");
+for (i = 0; i < nombreLigne; i++) {
+  affichage += nombre;
+  console.log(affichage);
+}
+onclick(btn){
+     document.bgColor= "#FFFggF"
+}
